5d74e8c804411769d886c9a634b74201df7f6338
Kheng <khenguzamiki1@gmail.com>
2025-02-18 10:51:29 +0700

    LibraryServices

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './Layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './Layouts/HomePage/ExploreTopBooks';
 import Carousel from './Layouts/HomePage/Carousel';
 import Hero from './Layouts/HomePage/Hero';
+import LibraryServices from './Layouts/HomePage/LibraryServices';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <ExploreTopBooks />
       <Carousel />
       <Hero />
+      <LibraryServices />
     </div>
   );
 }
