1e2bc17f16d559df334d7de060cff1484a8f8ae0
Kheng <khenguzamiki1@gmail.com>
2025-02-18 10:15:10 +0700

    SignleBook

--- src/Layouts/HomePage/Carousel.tsx
+++ src/Layouts/HomePage/Carousel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReturnBook from "./ReturnBook";
 
 const Carousel = () => {
   return (
@@ -17,63 +18,27 @@ const Carousel = () => {
           {/* First Slide (Active) */}
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="Book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>Author: John Doe</p>
-                  <a href="#" className="btn main-color text-white">
-                    Reserve
-                  </a>
-                </div>
-              </div>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
             </div>
           </div>
 
           {/* Second Slide */}
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="Book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>Author: John Doe</p>
-                  <a href="#" className="btn main-color text-white">
-                    Reserve
-                  </a>
-                </div>
-              </div>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
             </div>
           </div>
 
           {/* Third Slide */}
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="Book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>Author: John Doe</p>
-                  <a href="#" className="btn main-color text-white">
-                    Reserve
-                  </a>
-                </div>
-              </div>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
             </div>
           </div>
         </div>
@@ -108,21 +73,7 @@ const Carousel = () => {
       {/* Mobile */}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center align-items-center">
-          <div className="text-center">
-            <img
-              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-              width="151"
-              height="233"
-              alt="Book"
-            />
-            <h6 className="mt-2">
-              <b>Book</b>
-            </h6>
-            <p>Author: John Doe</p>
-            <a href="#" className="btn main-color text-white">
-              Reserve
-            </a>
-          </div>
+          <ReturnBook />
         </div>
       </div>
 
