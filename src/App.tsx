819aa8cb1522a737c24d62e8326eeb57598bfcaa
Kheng <khenguzamiki1@gmail.com>
2025-02-18 09:40:52 +0700

    add static and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome</h1>
-    </div>
+    <Navbar />
   );
 }
 
