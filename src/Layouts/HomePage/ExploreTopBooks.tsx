706e799077807d0945c72d9d67e661139eee91cb
Kheng <khenguzamiki1@gmail.com>
2025-02-18 09:46:28 +0700

    ExploreTopBooks

---
+++ src/Layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const ExploreTopBooks = () => {
+  return (
+    <div className="p-5 mb-4 bg-dark header">
+      <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+        <div>
+          <h1 className="display-5 fw-bold">Find Your next adventure</h1>
+          <p className="col-md-8 fs-4">Where would you like to go next?</p>
+          <a
+            type="button"
+            href="#"
+            className="btn main-color btn-lg text-white"
+          >
+            Exlore top books
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
