afd8de50515467155a5d6e5f50f2d7244f9a318b
Kheng <khenguzamiki1@gmail.com>
2025-02-18 15:38:17 +0700

    Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './Layouts/NavbarAndFooter/Navbar';
 import Footer from './Layouts/NavbarAndFooter/Footer';
 import HomePage from './Layouts/HomePage/HomePage';
+import SearchBookPage from './Layouts/SearchBookPage/SearchBookPage';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <HomePage />     
+      {/* <HomePage />*/}
+      <SearchBookPage />
       <Footer/>
     </div>
   );
